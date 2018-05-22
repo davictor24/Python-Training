d7490afc33fe730366ac260ea1bc7f780c8c5bca
Victor Durojaiye <durojaiyevic@gmail.com>
2018-05-22 11:29:22 +0100

    Code changes

---
+++ Tuesday/countdown.py
@@ -0,0 +1,11 @@
+print("Using a while loop")
+i = 10
+while i >= 1:
+    print(i)
+    i -= 1
+print("Blast off!")
+
+print("\nUsing a for.. in loop")
+for i in range(10, 0, -1):
+    print(i)
+print("Blast off!")
---
+++ Tuesday/multiplication_table.py
@@ -0,0 +1,4 @@
+for i in range(2, 13):
+    for j in range(1, 13):
+        print("{0} x {1} = {2}".format(i, j, i*j))
+    print()
